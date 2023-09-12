2eb2234df4dcfe13dc398b6bdd96e831d279a92f
MembriniLuca29 <lucamembrini29@gmail.com>
2023-09-12 17:28:23 +0200

    migration fatta

--- database/migrations/2023_09_12_144730_create_trains_table.php
+++ database/migrations/2023_09_12_144730_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 32);
+            $table->string('stazione di partenza', 64);
+            $table->string('stazione di arrivo', 64);
+            $table->time('orario di partenza', $precision = 0);
+            $table->time('orario di arrivo', $precision = 0);
+            $table->mediumInteger('codice treno');
+            $table->smallInteger('numero carrozze');
+            $table->boolean('in orario')->default(true);
+            $table->boolean('cancellato')->default(false);
             $table->timestamps();
         });
     }
